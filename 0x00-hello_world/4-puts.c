2dfecd8a57c2c9f195f3a093a5dca78dcc9141d7
phinafati <phinafati84@gmail.com>
2022-12-08 15:43:53 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-# include <stdio.h>
+#include <stdio.h>
 /**
  *  main - Entry
  *  Return: Always 0
  */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0)
-	Betty 4-puts.c 
-	
+puts("\"Programming is like building a multi lingual puzzle);
+return (0);
+}
