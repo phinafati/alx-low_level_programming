9d0e29d1d1c45f4d45e9b6b59037175fb8e6968d
phinafati <phinafati84@gmail.com>
2022-12-13 08:39:05 -0800

    print alphabet 10 times done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void);
 	int j;
 	int x;
 
-	for (x = 0; x < 10;  x++)
+	for `('x = 0; x < 10;  x++)
 	{
-		for (j = 'a'; j <= 'z';  j++)
+		for `('j = 'a'; j <= 'z';  j++)
 		{
 			putchar(j);
 		}
