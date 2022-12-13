7be524c4c65f6ad63df7ab96b6bc632d59e2385d
phinafati <phinafati84@gmail.com>
2022-12-13 12:47:59 -0800

    hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,14 @@
 void print_alphabet_x10(void);
 {
 	int j;
-int x;
-for (x = 0; x < 10;  x++)
-{
-for (j = 'a'; j <= 'z';  j++)
-{
-putchar(j);
-}
-_putchar('\n');
+        int x;
+
+        for (x = 0; x < 10;  x++)
+        {
+            for (j = 'a'; j <= 'z';  j++)
+	    {
+                 putchar(j);
+            }
+            _putchar('\n');
+       }
 }
