628e2f63eb6bc3909bd3102c3c78b9a047ea7390
phinafati <phinafati84@gmail.com>
2022-12-15 12:27:54 -0800

    file

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,22 +12,17 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
+if (a >= b && a >= c)
 {
 	largest = a;
 }
-else if (b > c && b > a)
+else if (b >= c && b >= a)
 {
 	largest = b;
 }
 else if (c > b)
 {
-	largest = c;
+largest = c;
 }
-else
-{
-	largest = b;
-}
-
 return (largest);
 }
