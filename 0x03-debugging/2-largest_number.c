1ca4d18500f663016b7b6b78bf9a312858a47c95
phinafati <phinafati84@gmail.com>
2022-12-17 09:24:18 -0800

    2 task

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,22 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a >= b && a >= c)
+if (a >= b && a > c)
 {
 	largest = a;
 }
-else if (b >= c && b >= a)
+else if (b > c && b > a)
 {
 	largest = b;
 }
 else if (c > b)
 {
-largest = c;
+        largest = c;
 }
+else
+{
+	largest =b;
+}
+
 return (largest);
 }
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-1-main.c
+2nd task
