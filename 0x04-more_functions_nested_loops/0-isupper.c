8ba1f5a370b3c5ac9cf5fd465d18459c18b2010f
phinafati <phinafati84@gmail.com>
2022-12-18 09:07:23 -0800

    first task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,14 +5,14 @@
  *   @c: input character.
  *   Return : l if is an uppercase character, 0 in order case.
  */
-int _isupper(int c) 
+int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
-{
-return (l);
-}
-else
-{
-return (0);
-}
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
