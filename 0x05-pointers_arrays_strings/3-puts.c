a562c29d914391c8ac36d7a28d21a9369c3b8417
phinafati <phinafati84@gmail.com>
2022-12-19 14:16:53 -0800

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - write a function that prints a string,followed  by a new line,to
+ * stdout.
+ *
+ * @str: this is my input string
+ *
+ */
+void _puts(char *str)
+{
+	int index;
+
+	for (index = 0; str[inex] != '\0'; index++)
+	{
+		_putchar(str[inex]);
+	}
+	putchar('\n);
+}
