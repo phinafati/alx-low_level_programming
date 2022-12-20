5bff192b98d955a005171d1483648e82feff1bdf
phinafati <phinafati84@gmail.com>
2022-12-20 10:31:40 -0800

    task8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,14 +7,14 @@
  *
  * Return: void.
  */
-void print_array(int *a, int n);
+void print_array(int *a, int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i != n = 1)
+		if (i != n - 1)
 		{
 			printf(",");
 			printf(" ");
