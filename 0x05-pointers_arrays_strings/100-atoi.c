29b218f44807ecb8289d4ca22d4177fcc830b59e
phinafati <phinafati84@gmail.com>
2022-12-20 02:16:08 -0800

    start

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,6 +13,7 @@ int _atoi(char *s)
 	unsigned int num;
 	char *tmp;
 
+
 	tmp = s;
 	num = 0;
 	sign = 1;
@@ -27,7 +28,7 @@ int _atoi(char *s)
 		do {
 			num = num * 10 + (*tmp - '0');
 			tmp++;
-		} while (*tmp >= '0' && *tmp <= '9');
+		}  while (*tmp >= '0' && *tmp <= '9');
 	}
 	return (num * sign);
 }			
