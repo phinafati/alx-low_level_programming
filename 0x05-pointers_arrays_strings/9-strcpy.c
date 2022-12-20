3a41f090d77b7b919b85483e61081944997bec80
phinafati <phinafati84@gmail.com>
2022-12-19 17:45:57 -0800

    correct

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strpy - copiar el contenido de uno a uno aa otra variable
+ * @dest: This is destiny
+ * @src: This is copia
+ *
+ * Return: This return copy
+ */
+char *_strcpy(char *dest, char *srcc)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		srcc++;
+	}
+	*dest = '\0';
+	return (stsrt);
+}
