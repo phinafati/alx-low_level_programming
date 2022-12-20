7dd8689d2a1dfa33f880cc8493c6cdc58e89a72e
phinafati <phinafati84@gmail.com>
2022-12-20 13:59:27 -0800

    Great leaders

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,14 +11,14 @@ int _atoi(char *s)
 {
 	int sign;
 	unsigned int num;
-	char *temp
-		
+	char *temp;
+
 	temp = s;
-	num = 0;
+	num 0;
 	sign = 1;
 	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
-		if (*temp == '-')
+		if (temp == '-')
 			sign *= -1;
 		temp++;
 	}
@@ -27,7 +27,9 @@ int _atoi(char *s)
 		do {
 			num = num * 10 + (*temp - '0');
 			temp++;
-		}  while (*temp >= '0' && *temp <= '9');
+		} while (*temp >= '0' && *temp <= '9');
 	}
 	return (num * sign);
-}	
+}
+
+
