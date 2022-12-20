aa019aaab6bb7848d90339b8d2226b5afbae250a
phinafati <phinafati84@gmail.com>
2022-12-19 19:38:31 -0800

    go

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords
+ * return: 0 (on success)
+ *
+ */
+
+int main(void)
+{
+
+	int pass, sum;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%c", pass);
+	}
+	printf("%c", 2772 - sum);
+
+	return (0);
+}
+
