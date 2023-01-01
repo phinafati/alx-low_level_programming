b6cb0b96bc44c7fad16859304b8ee9767fca2fe7
phinafati <phinafati84@gmail.com>
2022-12-31 22:10:34 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	printf("last digit of %i is %i and is greater than 5\n", n, last);
 	else if (last == 0)
 		printf("last digit of %i is %i and is 0\n", n, last);
-	else if (last <6)
-printf("last digit of %i is %i and is less than 6 and not 0\n", n, last);
+	else if (last < 6)
+	printf("last digit of %i is %i and is less than 6 and not 0\n", n, last);
 	return (0);
 }
