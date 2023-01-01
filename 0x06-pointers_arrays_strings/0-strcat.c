6d211dc0cfaf9a70577749a5d80ce90b522086b0
phinafati <phinafati84@gmail.com>
2023-01-01 01:17:50 -0800

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,24 +10,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+int a = -1, i;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
+for (i = 0; dest[i] != '\0')
+i++;
 
-	j = 0;
+do {
+	a++;
+	dest[i] = src[a];
+	i++;
 
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
+} while (src[j] != '\0');
 
-	dest[i] = '\0';
-	return (dest);
+return (dest);
+
 }
