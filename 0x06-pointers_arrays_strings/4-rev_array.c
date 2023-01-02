9801bfc22da63314a02226472740cb5be5509c0e
phinafati <phinafati84@gmail.com>
2023-01-02 15:47:44 -0800

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include  "main.h"
+
+/**
+ * reverse_array - reverse the content of an array of integers
+ * @a: passed array argument
+ * @n: size of an array
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, swp;
+
+	for (i = 0; (i < (n - 1) / 2); i++)
+	{
+		swp = a[i];
+		a[i] = a[n - 1 -i];
+		a[n - 1 - i] = swp;
+	}
+}
