1eccb5941e59fd2035dcd36438ca651c03704055
phinafati <phinafati84@gmail.com>
2023-01-02 06:18:53 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints exactly and that piece of art is useful
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	write(2, "piece of art useful\" - Dora korpa, 2015-10-19\n", 59);
+	return (1);
+}
