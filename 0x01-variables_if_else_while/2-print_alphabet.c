035488ad3844885692ce265fdda5b243d1a31819
phinafati <phinafati84@gmail.com>
2023-01-02 00:00:27 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,16 @@
  * Description: get a random it with 5
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 
 	char c = 'a';
+	
 	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
 	}
-
 	putchar('\n');
 	return (0);
 }
