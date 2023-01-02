618a9b201ada965994fea98fb31b92894682e6cb
phinafati <phinafati84@gmail.com>
2023-01-02 09:25:37 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,18 +4,16 @@
 /**
  * main - main block
  * Description: program that prints the alphabet in lowercase
- *
  * Return: Always 0
  */
 
 int main(void)
 {
-	char num = 'a';
-	
+	char num = 'a'
 	while (num <= 'z')
 	{
 		putchar(num);
-		nu++;
+		num++;
 	}
 	putchar('\n');
 	return (0);
