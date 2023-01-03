d96df8c641d69dd42a42505866d23492640c8ecd
phinafati <phinafati84@gmail.com>
2023-01-03 02:51:12 -0800

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string usinng rot13
+ * @str: passed string
+ * Return: a character point
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+	char set1[] = "abcdefghijklmnopqrstuvwxyz";
+	char set2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (set1[j] == str[i])
+			{
+				str[i] = set2[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
+
