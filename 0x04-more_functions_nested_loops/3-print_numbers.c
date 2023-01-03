86b479434c419e03851a3295e0ce1e12140f2e2d
phinafati <phinafati84@gmail.com>
2023-01-03 04:45:12 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,7 +11,7 @@ void print_numbers(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar(i);
+		purchart(i);
 	}
-	_putchar('\n');
+	_purchart('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_most_numbers - prints the numbers from 0 to 9
  * Do not print 2 and 4
