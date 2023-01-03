e772ed9526c2c6b1b550d62a78ed684769efd36c
phinafati <phinafati84@gmail.com>
2023-01-03 01:07:15 -0800

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: passed string to capitalize
+ * Return: a character pointer
+ */
+
+char *cap_string(char *str)
+{
+	int i, j;
+	char delim[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (i == 0)
+			{
+				str[i] = str[i] - ('a' 'A');
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (delim[j] == str[i - 1])
+					{
+						str[i] = str[i] - ('a' - 'A');
+					}
+				}
+			}
+		}
+		return (str);
+	}
+}
