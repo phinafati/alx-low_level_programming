1b32c4a7e6239ec26279d0ffadefcc7719d2125e
phinafati <phinafati84@gmail.com>
2023-01-02 17:30:24 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
-
 /**
-* _isupper - check if numbers are 0 - 9
-* @c: char to check
-* return: 0 or 1
-*
-*/
+ * _isdigit - check if an argument is digit
+ * @c: an integer argument
+ * Return: 0 or 1
+ */
 
-
-int _isupper(int c)
+int _isdigit(int c)
 {
-
-if (c >= '0' && c <= '9')			
-return (1);
-else
-return (0);
-
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
