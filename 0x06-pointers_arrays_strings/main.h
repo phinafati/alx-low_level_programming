88535c24c1d8bc33a4d74c4d47bb1d6fba281c5f
phinafati <phinafati84@gmail.com>
2023-01-03 00:11:32 -0800

    first

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,7 @@
-#ifndef FILE_MAIN
-
-#define FILE_MAIN
-
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
-
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -17,6 +14,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
 
 #endif
