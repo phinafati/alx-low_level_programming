7071b592a28d8d3bbc3445d6c93f9d92608e1978
phinafati <phinafati84@gmail.com>
2023-01-04 11:27:45 -0800

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -  entry point
+ * Description: prints the largest prime factor of the number
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int num, pfac;
+
+	num = 6128552475143;
+	for (pfac = 2; pfac <= num; pfac++)
+	{
+		if (num % pfac == 0)
+		{
+			num = num / pfac;
+			pfac--;
+		}
+	}
+	printf("%ld\n", pfac);
+	return (0);
+}
