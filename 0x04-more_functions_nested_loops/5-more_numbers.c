acbc0f55b239e0a36df0e131c05faaac2625ab07
phinafati <phinafati84@gmail.com>
2023-01-04 12:10:27 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,8 +15,8 @@ void more_numbers(void)
 
 		for (j = 0; j <= 14; j++)
 		{
-			if (j >= 10) _putchar('1');
-
+			if (j >= 10)
+				_putchar('1');
 			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
