3b39054bc5ce99038a20833ed5e86674bfbcce67
phinafati <phinafati84@gmail.com>
2023-01-04 10:57:55 -0800

    task8

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: write program that prints the numbres from 1 to 100
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
+		else if ((i % 3) == 0)
+			printf("Fizz");
+		else if ((i % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			print(" ");
+	}
+	prinntf("\n");
+	return (0);
+}
+
+
