650e2ce8c3905567b31ad571076639be52555c8f
phinafati <phinafati84@gmail.com>
2023-01-05 23:01:43 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int 1 = 0;
+	unsigned int i = 0;
 
-	for (; n > 0; i++)
+	while (i < n)
 	{
-		s[i] = b;
-		n--;
+		*(s + i) = b;
+		i++;
 	}
 	return (s);
 }
