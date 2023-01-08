0245470d65269c81fcf34133c5ff45955a7d7756
phinafati <phinafati84@gmail.com>
2023-01-08 02:16:43 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,13 @@
 
 void _puts_recursion(char *s)
 {
-
-	if (*s == '\0')
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
+	s = "lpa\0";
+		if (*s == '\0')
+		{
+			_putchar('\n');
+			return;
+		}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
