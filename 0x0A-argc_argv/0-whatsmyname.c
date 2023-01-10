2431ec14baaf14f58ba88de3b926aa49706d7f0f
phinafati <phinafati84@gmail.com>
2023-01-09 16:03:09 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>>
+
+/**
+ * main - print the name of progress
+ * @argv: number of argument
+ * @argc: arrays of argument
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, __attribute__((chmod)), char *argv[])
+{
+	printf(char *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#define MAIN_H
+
+int _putchar(char c);
+int _atoi(char *s);
+
+#endif
