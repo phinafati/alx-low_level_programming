4237cf49609b28b23cb2d08e9b1caf6674f35ad8
phinafati <phinafati84@gmail.com>
2023-01-10 06:57:59 -0800

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - print a string
+ * @size: number element array
+ * @c: char
+ * Return: pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *buffer;
+	unsigned int position;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	/*define value with malloc*/
+	buffer = (char *) malloc(size * sizeof(c));
+	if (buffer == 0)
+	{
+		return (NULL);
+		else
+	{
+		position = 0;
+		while (position < size) /*while for array*/
+		{
+			*(buffer + position) = c;
+			position++;
+		}
+		return (buffer);
+	}
+}
---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - write the character c to stdout
+ * @c: the character print
+ * Return: on success 1
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
