5d0c01785b29196beb5a8b30f97d49a6018e2f62
phinafati <phinafati84@gmail.com>
2023-01-13 02:33:48 -0800

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - allocated memory uusing mallocc
+ * @b: size of memory to be allocated
+ *
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p  malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - write the character c to stdout
+ * @c: The character to print
+ *
+ * On success 1
+ * On error, -1 is returned , and error is a set of aproprietely
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
