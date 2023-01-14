2712bd7d0c208571c4a796ca302b256453838c4b
phinafati <phinafati84@gmail.com>
2023-01-14 11:02:23 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
+
 /**
  * _puts_recursion - prints a string followed by a new putchar
  * @s: pointer block of memory to fill
+ *
  * Return: void
  */
 
 void _puts_recursion(char *s)
 {
-	s = "lpa\0";
-		if (*s == '\0')
-		{
-			_putchar('\n');
-			return;
-		}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar (*s);
+	_puts_recursion(s + 1);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,5 @@
 #ifndef _MAIN_H
 #define _MAIN_H
-
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
