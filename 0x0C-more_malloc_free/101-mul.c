dd17ee4ede7f6875a16e20cfc1e763d4cd14a133
phinafati <phinafati84@gmail.com>
2023-01-14 09:40:28 -0800

    2-calloc.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -29,7 +29,7 @@ int is_digit(char *s)
  */
 int _strlen(char *s)
 {
-	int i = o;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
@@ -61,11 +61,11 @@ int main(int argc, char *argv)
 		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
+	len2 = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len2);
 	if (!result)
 		return (1);
-	for (i = 0; i <= len1 + len2, i++)
+	for (i = 0; i <= len1 + len2, i++);
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
@@ -78,7 +78,7 @@ int main(int argc, char *argv)
 			carry /= 10;
 		}
 		if (carry > 0)
-			resuult[len1 + len2 + 1] += carry;
+			result[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
 	{
@@ -94,4 +94,3 @@ int main(int argc, char *argv)
 	return (0);
 
 }
-
