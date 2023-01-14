e8cad0180c9a6ea0806a01fcbd327f9ce8160298
phinafati <phinafati84@gmail.com>
2023-01-14 10:33:18 -0800

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,6 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 #define ERR_MSG "Error"
 
 /**
@@ -9,7 +9,7 @@
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
-int is_digit(char *s)
+int is_digit(int *s)
 {
 	int i = 0;
 
@@ -27,6 +27,7 @@ int is_digit(char *s)
  *
  * Return: the length of the string
  */
+
 int _strlen(char *s)
 {
 	int i = 0;
@@ -51,18 +52,18 @@ int _strlen(char *s)
  *
  * Return: always 0 (success)
  */
-int main(int *argc, char *argv)
+int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, i, carry, digit1, digit2, *result, a = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s2))
+	if (argc != 3 || !is_digit(1) || !is_digit(2))
 		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
-	len2 = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2, i++;)
@@ -85,7 +86,7 @@ int main(int *argc, char *argv)
 		if (result[i])
 			a = 1;
 		if (a)
-			_putchar(result[i] + '0');
+			putchar(result[i] + '0');
 	}
 	if (!a)
 		_putchar('0');
