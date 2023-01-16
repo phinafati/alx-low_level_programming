f7c5365ddfd96d3c95495e3f6a907404b7e42a9d
phinafati <phinafati84@gmail.com>
2023-01-16 00:38:05 -0800

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,13 +1,19 @@
-#ifndef _MAIN_H
-#define _MAIN_H
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
+#include "main.h"
 
-#endif
+/**
+ * _puts_recursion - function that prints a string
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar (*s);
+	_puts_recursion(s + 1);
+}
