260ab01319422a17741a4b06cf64af74d848365a
phinafati <phinafati84@gmail.com>
2023-01-16 13:32:39 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,16 +2,15 @@
 
 /**
  * main - Prints the sum of all multiples of 3 or 5 up to 1024
- * Return: 0
+ * Return: Always (Success)
  */
-
-int main(void);
+int main(void)
 {
 	int i, z = 0;
 
 	while (i < 1024)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
 			z += i;
 		}
