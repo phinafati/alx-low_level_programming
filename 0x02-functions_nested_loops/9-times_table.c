b95c38fcb052dcf520a0894a73591f2304fd4266
phinafati <phinafati84@gmail.com>
2023-01-21 23:04:51 -0800

    task3

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,18 +18,18 @@ void times_table(void)
 				
 				if (b == 0)
 				{
-					_putchar('0');
+				_putchar('0');
 				}
 
 				if (b != 0)
 				{
-					_putchar(' ');
-					_putchar((c % 10) + '0');
+				_putchar(' ');
+				_putchar((c % 10) + '0');
 				}
 				if (b < 9)
 				{
-					_putchar(',');
-					_putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 				}
 
 			}
@@ -39,8 +39,8 @@ void times_table(void)
 				_putchar((c % 10) + '0');
 				if (b < 9)
 				{
-					_putchar(',');
-					_putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 				}
 			}
 		}
