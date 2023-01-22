ae900b70823640d143651462714245af201e1ce1
phinafati <phinafati84@gmail.com>
2023-01-21 22:21:33 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type of data
+ * representing a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner's nam/
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG_H*/
