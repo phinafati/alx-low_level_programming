35f21ece62b862f9724cf4b574775e69bc1a4dde
phinafati <phinafati84@gmail.com>
2023-01-24 01:53:04 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,19 +3,22 @@
 /**
  * print_number - prints an integer
  * @n: number to print
+ *
  * Return: void
  */
-
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int x;
 
 	if (n < 0)
 	{
-		_putchar('_');
-		num = -num;
+		_putchar('-');
+		n *= -1;
 	}
-	if ((num / 10) > 0)
-		print_number(num / 10);
-	_putchar((num % 10) + '0');
+
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+	_putchar(x % 10 + '0');
 }
