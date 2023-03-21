698dde1eeb866aa8fc186fc9ea8ff50d7a50640a
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-03-20 23:46:12 -0700

    0-isupper.c

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+int main()
+{
+
+}
+/**
+ * _puts - write a function that prints a string, followed by a new line, to
+ * stdout.
+ *
+ * @str: This is my input string
+ *
+ */
+void _puts(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
