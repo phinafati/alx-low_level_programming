5b93b37e2abba472c36828c6ba0def9c7044e3fa
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-04-14 06:12:23 -0700

    1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,20 +1,17 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
+ * hash_djb2 - Hash funmction implementing the djb2 algorithm
+ * @str: The string the hash
  *
- * Return: hash value
+ * Return: The calculated hash.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
+unsigned long int hash;
+int c;
+hash = 5381;
+while ((c = *str++))
+hash = ((hash << 5) + hash) +c; /* hash * 33 + c */
+return (hash);
 }
